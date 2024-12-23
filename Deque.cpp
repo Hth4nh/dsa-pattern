dd5c5755cab96c914f70fef3b1808d9043291b7d
Thành Hoàng Trần <hth4nh@gmail.com>
2024-12-23 19:54:09 +0700

    Update Deque.cpp

--- Deque.cpp
+++ Deque.cpp
@@ -5,11 +5,11 @@ template <typename T>
 class Deque {
 private:
     T* arr;
-    size_t size, front, back;
+    size_t n, start, end;
 
 public:
-    Deque(int n) : size(n + 2), front(0), back(size - 1) {
-        arr = new T[size];
+    Deque(int size) : n(size + 2), start(0), end(n - 1) {
+        arr = new T[n];
     }
 
     ~Deque() {
@@ -17,68 +17,73 @@ public:
     }
 
     bool empty() const {
-        return front == (back + 1) % size;
+        return start == (end + 1) % n;
     }
 
     bool full() const {
-        return front == (back + 2) % size;
+        return start == (end + 2) % n;
     }
 
     void push_front(const T& value) {
         if (full()) throw std::overflow_error("Deque is full");
-        front = (front - 1 + size) % size;
-        arr[front] = value;
+        start = (start - 1 + n) % n;
+        arr[start] = value;
     }
 
     void push_back(const T& value) {
         if (full()) throw std::overflow_error("Deque is full");
-        back = (back + 1) % size;
-        arr[back] = value;
+        end = (end + 1) % n;
+        arr[end] = value;
     }
 
     void pop_front() {
         if (empty()) throw std::underflow_error("Deque is empty");
-        front = (front + 1) % size;
+        start = (start + 1) % n;
     }
 
     void pop_back() {
         if (empty()) throw std::underflow_error("Deque is empty");
-        back = (back - 1 + size) % size;
+        end = (end - 1 + n) % n;
     }
 
     T front() const {
         if (empty()) throw std::underflow_error("Deque is empty");
-        return arr[front];
+        return arr[start];
     }
 
     T back() const {
         if (empty()) throw std::underflow_error("Deque is empty");
-        return arr[back];
+        return arr[end];
     }
 
     size_t size() const {
-        if (front <= back) {
-            return back - front + 1;
+        if (start <= end) {
+            return end - start + 1;
         } else {
-            return size - front + back + 1;
+            return n - start + end + 1;
         }
     }
 
     size_t capacity() const {
-        return size - 2;
+        return n - 2;
+    }
+
+    void clear() {
+        start = 0;
+        end = n - 1;
     }
 
     T& operator[](size_t index) {
         if (index >= size()) {
             throw std::out_of_range("Index out of range");
         }
-        return arr[(front + index) % size];
+        return arr[(start + index) % n];
     }
 
     const T& operator[](size_t index) const {
         if (index >= size()) {
             throw std::out_of_range("Index out of range");
         }
-        return arr[(front + index) % size];
+        return arr[(start + index) % n];
     }
 };
